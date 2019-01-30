a053fd13b68ddb384f6f720aa863395124574a08
mark-marshall <mark.marshallgp@gmail.com>
2019-01-30 14:13:59 +0000

    [STRETCH] Added menu event handler so menu stays open when clicked on

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,7 +3,7 @@
 const body = document.querySelector('body');
 console.log(body);
 
-const closeMenu = e => {
+const closeMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   //console.dir(e.target);
   menu.classList.remove('menu--open');
@@ -17,9 +17,16 @@ const toggleMenu = e => {
   e.stopPropagation();
 };
 
+const keepMenuOpen = e => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.add('menu--open');
+  e.stopPropagation();
+};
+
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+menu.addEventListener('click', keepMenuOpen);
