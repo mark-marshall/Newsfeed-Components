3fa06abbdbf11d9a4b35b69e0af55219a4c77184
mark-marshall <mark.marshallgp@gmail.com>
2019-01-30 14:10:39 +0000

    [STRETCH] Enabled body event handler to collapse menu and stopped propogation to menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,20 @@
-const toggleMenu = () => {
+//Testing new event handlerr
+
+const body = document.querySelector('body');
+console.log(body);
+
+const closeMenu = e => {
+  // Toggle the "menu--open" class on your menu refence.
+  //console.dir(e.target);
+  menu.classList.remove('menu--open');
+};
+
+body.addEventListener('click', closeMenu);
+
+const toggleMenu = e => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
+  e.stopPropagation();
 };
 
 // Start Here: Create a reference to the ".menu" class
