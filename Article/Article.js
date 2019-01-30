21ed499184c2815368fc9769ba51e9748e85db5d
mark-marshall <mark.marshallgp@gmail.com>
2019-01-30 15:30:43 +0000

    [STRETCH] Added Click to Expand and Click to Close logic on expandButton

--- Article/Article.js
+++ Article/Article.js
@@ -15,6 +15,11 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.article.classList.toggle('article-open');
+    if (this.article.classList.length === 2) {
+      this.expandButton.innerText = 'Click to Close';
+    } else {
+      this.expandButton.innerText = 'Click to Expand';
+    }
   }
 }
 
